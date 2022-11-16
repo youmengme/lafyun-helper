937fa4f41531a63c59c399d43c4126fc667aceeb
youmeng <1410180317@qq.com>
2022-11-16 16:40:05 +0800

    1

---
+++ index.js
@@ -0,0 +1,23 @@
+const boxStyle = {
+  position: 'fixed',
+  left: 0,
+  top: 0,
+  bottom: 0,
+  width: '300px',
+  zIndex: 100,
+  background: '#fff',
+}
+
+function joinStyles(styleObject) {
+  return Object.entries(styleObject).map(([key, value]) => `${key}: ${value}`).join(';')
+}
+
+function lafyun () {
+  const body = document.querySelector('body')
+  if (!body) return
+  const box = document.createElement('div')
+  box.class = 'box show'
+  body.appendChild(box)
+}
+
+lafyun()
